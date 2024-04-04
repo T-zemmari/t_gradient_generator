41f65b0c3e74085316c323a5f7689f4482b92d27
Tarek <tarek@tedybusiness.com>
2024-04-04 16:15:52 +0200

    ahora se puede generar tanto un gradiente lineal o un gradiente radial

--- src/assets/Componentes/LandingPage/LandingPage.jsx
+++ src/assets/Componentes/LandingPage/LandingPage.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
-import { BsArrowUp, BsArrowDown, BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import {
+  BsArrowUp,
+  BsArrowDown,
+  BsArrowLeft,
+  BsArrowRight,
+} from "react-icons/bs";
 
 const GradientGenerator = () => {
   const [gradientCode, setGradientCode] = useState(
@@ -9,13 +14,19 @@ const GradientGenerator = () => {
   const [endColor, setEndColor] = useState("#E28136");
   const [direction, setDirection] = useState("to right");
   const [gradientType, setGradientType] = useState("linear");
+  const [radialPositionTopColor, setRadialPositionTopColor] = useState(
+    "#e66465"
+  );
+  const [radialPositionBottomColor, setRadialPositionBottomColor] = useState(
+    "#4d9f0c"
+  );
 
   const handleGradientChange = () => {
     let newGradient;
     if (gradientType === "linear") {
       newGradient = `linear-gradient(${direction}, ${startColor}, ${endColor})`;
     } else {
-      newGradient = `radial-gradient(circle, ${startColor}, ${endColor})`;
+      newGradient = `radial-gradient(ellipse at top, ${radialPositionTopColor}, transparent), radial-gradient(ellipse at bottom, ${radialPositionBottomColor}, transparent)`;
     }
     setGradientCode(newGradient);
   };
@@ -26,28 +37,66 @@ const GradientGenerator = () => {
   };
 
   const handleColorChange = (color, isStartColor) => {
-    isStartColor ? setStartColor(color) : setEndColor(color);
+    if (gradientType === "linear") {
+      isStartColor ? setStartColor(color) : setEndColor(color);
+    } else {
+      isStartColor
+        ? setRadialPositionTopColor(color)
+        : setRadialPositionBottomColor(color);
+    }
     handleGradientChange();
   };
 
   const DirectionIcon = ({ direction }) => {
     switch (direction) {
       case "to top":
-        return <BsArrowUp style={{color:'black',fontSize:'25px'}}/>;
+        return <BsArrowUp style={{ color: "black", fontSize: "25px" }} />;
       case "to bottom":
-        return <BsArrowDown style={{color:'black',fontSize:'25px'}}/>;
+        return <BsArrowDown style={{ color: "black", fontSize: "25px" }} />;
       case "to left":
-        return <BsArrowLeft style={{color:'black',fontSize:'25px'}}/>;
+        return <BsArrowLeft style={{ color: "black", fontSize: "25px" }} />;
       case "to right":
-        return <BsArrowRight style={{color:'black',fontSize:'25px'}}/>;
+        return <BsArrowRight style={{ color: "black", fontSize: "25px" }} />;
       case "to top left":
-        return <BsArrowUp style={{ transform: "rotate(-45deg)",color:'black',fontSize:'25px' }} />;
+        return (
+          <BsArrowUp
+            style={{
+              transform: "rotate(-45deg)",
+              color: "black",
+              fontSize: "25px",
+            }}
+          />
+        );
       case "to top right":
-        return <BsArrowUp style={{ transform: "rotate(45deg)",color:'black',fontSize:'25px' }} />;
+        return (
+          <BsArrowUp
+            style={{
+              transform: "rotate(45deg)",
+              color: "black",
+              fontSize: "25px",
+            }}
+          />
+        );
       case "to bottom left":
-        return <BsArrowDown style={{ transform: "rotate(45deg)" ,color:'black',fontSize:'25px'}} />;
+        return (
+          <BsArrowDown
+            style={{
+              transform: "rotate(45deg)",
+              color: "black",
+              fontSize: "25px",
+            }}
+          />
+        );
       case "to bottom right":
-        return <BsArrowDown style={{ transform: "rotate(-45deg)",color:'black',fontSize:'25px' }} />;
+        return (
+          <BsArrowDown
+            style={{
+              transform: "rotate(-45deg)",
+              color: "black",
+              fontSize: "25px",
+            }}
+          />
+        );
       default:
         return null;
     }
@@ -63,56 +112,100 @@ const GradientGenerator = () => {
         className="w-full max-w-lg bg-[#ffffff40] rounded-lg shadow-md p-6"
         id="container_controls"
       >
-        <div className="flex flex-row justify-between">
-          <div className="mb-4 flex flex-col justify-center items-center">
-            <label
-              htmlFor="start-color"
-              className="block text-[#8b2f2f] font-bold mb-2"
-            >
-              START
-            </label>
-            <input
-              id="start-color"
-              type="color"
-              className="rounded-md py-1 px-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              value={startColor}
-              onChange={(e) => handleColorChange(e.target.value, true)}
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="end-color"
-              className="block text-[#8b2f2f] font-bold mb-2"
-            >
-              END
-            </label>
-            <input
-              id="end-color"
-              type="color"
-              className="rounded-md py-1 px-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              value={endColor}
-              onChange={(e) => handleColorChange(e.target.value, false)}
-            />
-          </div>
-        </div>
         <div className="mb-4 ">
           <div className="flex items-center justify-between">
             <div
-              className="flex items-center gap-1 cursor-pointer flex-col"
+              className={`flex items-center gap-1 cursor-pointer flex-col ${
+                gradientType === "linear" ? "text-blue-500" : ""
+              }`}
               onClick={() => setGradientType("linear")}
             >
               <span>Linear</span>
             </div>
             <div
-              className="flex items-center gap-1 cursor-pointer flex-col"
+              className={`flex items-center gap-1 cursor-pointer flex-col ${
+                gradientType === "radial" ? "text-blue-500" : ""
+              }`}
               onClick={() => setGradientType("radial")}
             >
               <span>Radial</span>
             </div>
           </div>
         </div>
+        {gradientType === "radial" && (
+          <>
+            <div className="mb-4 flex flex-col justify-center items-center">
+              <label
+                htmlFor="radial-top-color"
+                className="block text-[#8b2f2f] font-bold mb-2"
+              >
+                Radial Position Top Color
+              </label>
+              <input
+                id="radial-top-color"
+                type="color"
+                className="rounded-md py-1 px-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                value={radialPositionTopColor}
+                onChange={(e) => handleColorChange(e.target.value, true)}
+              />
+            </div>
+            <div className="mb-4 flex flex-col justify-center items-center">
+              <label
+                htmlFor="radial-bottom-color"
+                className="block text-[#8b2f2f] font-bold mb-2"
+              >
+                Radial Position Bottom Color
+              </label>
+              <input
+                id="radial-bottom-color"
+                type="color"
+                className="rounded-md py-1 px-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                value={radialPositionBottomColor}
+                onChange={(e) => handleColorChange(e.target.value, false)}
+              />
+            </div>
+          </>
+        )}
+
+        <div className="flex flex-row justify-between">
+          {!["radial"].includes(gradientType) && (
+            <>
+              <div className="mb-4 flex flex-col justify-center items-center">
+                <label
+                  htmlFor="start-color"
+                  className="block text-[#8b2f2f] font-bold mb-2"
+                >
+                  START
+                </label>
+                <input
+                  id="start-color"
+                  type="color"
+                  className="rounded-md py-1 px-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  value={startColor}
+                  onChange={(e) => handleColorChange(e.target.value, true)}
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="end-color"
+                  className="block text-[#8b2f2f] font-bold mb-2"
+                >
+                  END
+                </label>
+                <input
+                  id="end-color"
+                  type="color"
+                  className="rounded-md py-1 px-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  value={endColor}
+                  onChange={(e) => handleColorChange(e.target.value, false)}
+                />
+              </div>
+            </>
+          )}
+        </div>
+        
         <div className="mb-4 ">
-          <div className="flex items-center justify-between">
+         {gradientType && gradientType === "linear" && ( <div className="flex items-center justify-between">
             {[
               "to top",
               "to bottom",
@@ -128,11 +221,12 @@ const GradientGenerator = () => {
                 className="flex items-center gap-1 cursor-pointer flex-col"
                 onClick={() => handleDirectionChange(dir)}
               >
-                <DirectionIcon direction={dir}/>
+                <DirectionIcon direction={dir} />
                 {/*<span>{dir}</span>*/}
               </div>
             ))}
-          </div>
+          </div>   
+        )}
         </div>
       </div>
     </div>
