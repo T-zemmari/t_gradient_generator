4bdf5872b28f7c25ea2f5f9aa3a2ab5344dbe822
Tarek <tarek@tedybusiness.com>
2024-04-04 08:04:44 +0200

    Landing page

--- src/assets/Componentes/LandingPage/LandingPage.jsx
+++ src/assets/Componentes/LandingPage/LandingPage.jsx
@@ -24,38 +24,46 @@ const GradientGenerator = () => {
   };
 
   return (
-    <div className={`w-full min-h-screen p-8 bg-gray-100 flex flex-col justify-center items-center`} style={{background:`${gradientCode}`}}>
+    <div
+      className={`w-full min-h-screen p-8 bg-gray-100 flex flex-col justify-end items-end`}
+      style={{ background: `${gradientCode}` }}
+    >
       <h1 className="text-3xl font-bold text-gray-800 mb-4"></h1>
-      <div className="w-full max-w-lg bg-white rounded-lg shadow-md p-6">
-        <div className="mb-4">
-          <label
-            htmlFor="start-color"
-            className="block text-gray-700 font-bold mb-2"
-          >
-            Start Color:
-          </label>
-          <input
-            id="start-color"
-            type="color"
-            className="rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            value={startColor}
-            onChange={(e) => handleColorChange(e.target.value, true)}
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="end-color"
-            className="block text-gray-700 font-bold mb-2"
-          >
-            End Color:
-          </label>
-          <input
-            id="end-color"
-            type="color"
-            className="rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            value={endColor}
-            onChange={(e) => handleColorChange(e.target.value, false)}
-          />
+      <div
+        className="w-full max-w-lg bg-white rounded-lg shadow-md p-6"
+        id="container_controls"
+      >
+        <div className="flex flex-row gap-2">
+          <div className="mb-4">
+            <label
+              htmlFor="start-color"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              Start
+            </label>
+            <input
+              id="start-color"
+              type="color"
+              className="rounded-md py-1 px-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              value={startColor}
+              onChange={(e) => handleColorChange(e.target.value, true)}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="end-color"
+              className="block text-gray-700 font-bold mb-2"
+            >
+              End
+            </label>
+            <input
+              id="end-color"
+              type="color"
+              className="rounded-md py-1 px-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              value={endColor}
+              onChange={(e) => handleColorChange(e.target.value, false)}
+            />
+          </div>
         </div>
         <div className="mb-4">
           <label
@@ -80,10 +88,6 @@ const GradientGenerator = () => {
             <option value="to bottom right">To Bottom Right</option>
           </select>
         </div>
-        <div
-          className="w-full h-32 rounded-md mb-4"
-          style={{ background: gradientCode }}
-        ></div>
       </div>
     </div>
   );
